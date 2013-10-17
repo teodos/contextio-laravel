9c2e9e50ee6fac1be41bf392a6896d0d12e093b6
Teddy Pejoski <teddy@g6Solutions.com>
2013-10-18 00:09:52 +0200

    Initial import

---
+++ src/Teddy/Contextio/ContextIOException.php
@@ -0,0 +1,13 @@
+<?php namespace Teddy\Contextio;
+
+/*
+	Context.IO Laravel 4 package build on top of the PHP Library for Context.IO
+	
+	Original PHP Library for Context.IO => https://github.com/dominikgehl
+
+	@author Teddy, teddy@g6solutions.com
+*/
+
+class ContextIOException extends \Exception {
+
+}
